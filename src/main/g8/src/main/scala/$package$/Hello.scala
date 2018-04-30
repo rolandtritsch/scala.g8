e1e1a3e5777a8598b91dd2cc3d41ccaad4443416
Roland Tritsch <rtritsch@tenable.com>
2018-04-30 16:15:20 +0100

    Showing how to escape the dollar sign when you do string interpolation (in the template code; kind of ugly :()

--- src/main/g8/src/main/scala/$package$/Hello.scala
+++ src/main/g8/src/main/scala/$package$/Hello.scala
@@ -1,13 +1,36 @@
 package $package$
 
-/** [[Hello]] object
+/** [[$package$.Hello]] object.
+  *
+  * Defines the [[main]] function and the [[greeting]] function.
+  *
+  * @see See [[https://docs.scala-lang.org/overviews/scaladoc/overview.html Scaladoc]] for more information on how to write scala documentation.
+  * @note This is by no means complete. It is just to illustrate how this (scala docs) works.
+  * @todo Add more documentation, with more tags (for demonstration purposes).
   */
 object Hello {
-  def greeting(name: String): String = {
-    "hello, " + name
-  }
 
+  /** The [[greeting]] function.
+    *
+    * Take the input string (a/the name of a person or a thing) and append it to 'hello, '.
+    *
+    * @note I know, that it violates DNRY to write documentation like this. The documentation should describe what a function is doing, not how it does it.
+    *
+    * @param name The name to append
+    *
+    * @return The full string
+    */
+  def greeting(name: String): String = {
+    require(name.nonEmpty, "name.nonEmpty failed")
+    s"\${prefix},  \${name}"
+  } ensuring(result => result.nonEmpty && result.size > name.size)
+
+  /** The [[main]] function
+    *
+    * Entry point. Just to call [[greeting]].
+    */
   def main(args: Array[String]): Unit = {
+    require(args.isEmpty, s"args.isEmpty failed; with >\${args.toList}<")
     println(greeting("world"))
   }
 }
