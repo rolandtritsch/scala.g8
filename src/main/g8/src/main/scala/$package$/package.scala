67af469e007d58d6dda651c7e7de8caae3a794b1
Roland Tritsch <roland@tritsch.org>
2018-05-28 19:38:09 +0100

    Breaking package into domain and project to make package.scala work

--- src/main/g8/src/main/scala/$package$/package.scala
+++ src/main/g8/src/main/scala/$package$/package.scala
@@ -1,11 +1,11 @@
-package org.tritsch
+package $domain$
 
 /** [[$package$]] object.
   *
   * This package implements a [[Hello HelloWorld]] object (and all tests for it).
   */
 
-package object example {
+package object $project$ {
   /** The prefix that is used in the implementation of the [[Hello.greeting]].
     */
   val prefix = "hello"
